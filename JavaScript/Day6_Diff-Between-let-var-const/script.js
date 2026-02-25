4e174f9bdd6336c8b381bcab05c9b045760da229
shanu-react-dev <amarjeet.kr.dev@gmail.com>
2026-02-25 13:04:29 +0530

    Difference between var let and const

---
+++ JavaScript/Day6_Diff-Between-let-var-const/script.js
@@ -0,0 +1,42 @@
+console.log("Shanu Singh is a worst person");
+
+//! var Keyword in JS
+// var myName = "Sunfeast Cookies"; //Declaration and initialization is possible at the same time
+// console.log(myName);
+// var myFavCar; //only declaration is possible
+// console.log(myFavCar);
+// myFavCar = "Dodge Challenger HellCat SRT Demon"; //ONly Initialization is also possible
+// console.log(myFavCar);
+// var myFavCar; // Redeclaration is possible using var keyword
+// console.log(myFavCar);
+// myFavCar = "Supra MK4"; //Reinitialization is also possible in var keyword
+// console.log(myFavCar);
+
+// var myFavCar = "BMW M4 Competitiion Edition";
+// console.log(myFavCar);
+
+// console.log(myFavFruit);
+// var myFavFruit;
+// console.log(myFavFruit);
+
+//! let keyword in JS
+// console.log(myFavPerson); //! Uncaught reference error
+let myFavPerson;
+console.log(myFavPerson);
+myFavPerson = "Mahendra Bahubali";
+console.log(myFavPerson);
+let myIgnoredPerson = "PM Modi";
+console.log(myIgnoredPerson);
+
+// let myFavPerson; //! Cannot redeclare block-scoped variable 'myFavPerson'.
+myFavPerson = "Katappa";
+console.log(myFavPerson);
+
+//! const Keyword in JS
+// const myFrndName;
+// myFrndName = "Sundari";
+
+const myFrndName = "Sundari";
+console.log(myFrndName);
+myFrndName = "Siva";
+// console.log(myFrndName); //!Uncaught TypeError: Assignment to constant variable.
